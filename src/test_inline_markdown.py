4cf6b81c82aec03e179d81ba2b280a5b1a28740e
Nathaniel Tabacchi <ntabacchi@outlook.com>
2024-06-04 23:12:36 -0400

    turning markdown text into TextNodes.

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -6,6 +6,7 @@ from inline_markdown import (
     extract_markdown_links,
     split_nodes_image,
     split_nodes_link,
+    text_to_textnodes,
 )
 
 from textnode import (
@@ -162,6 +163,23 @@ class TestInlineMarkdown(unittest.TestCase):
             TextNode(" is a great text editor for the tinkerer.", text_type_text),
         ]
         actual = split_nodes_link([node])
+        self.assertListEqual(expected, actual)
+
+    def test_text_to_textnodes(self):
+        text = "This is **text** with an *italic* word and a `code block` and an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and a [link](https://boot.dev)"
+        expected = [
+            TextNode("This is ", text_type_text),
+            TextNode("text", text_type_bold),
+            TextNode(" with an ", text_type_text),
+            TextNode("italic", text_type_italic),
+            TextNode(" word and a ", text_type_text),
+            TextNode("code block", text_type_code),
+            TextNode(" and an ", text_type_text),
+            TextNode("image", text_type_image, "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"),
+            TextNode(" and a ", text_type_text),
+            TextNode("link", text_type_link, "https://boot.dev"),
+        ]
+        actual = text_to_textnodes(text)
         self.assertListEqual(expected, actual)
 
 
